ab19c120046e76a8780b357866cb03cf33865879
Emily Goeres <goeres123@hotmail.com>
2020-11-09 17:46:10 -0700

    Added heroku deployment info and rearranged files

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// Add this to deploy to heroku
+const PORT = process.env. PORT || 3000;
 
 const app = express();
 
@@ -15,7 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Heroku deployment line 20
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
